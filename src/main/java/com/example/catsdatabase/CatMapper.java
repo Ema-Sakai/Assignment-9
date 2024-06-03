c0e0a38e1cbe0403d8fb570d51ac59a4bc033e5e
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-04 01:39:02 +0900

    DBのCatテーブルのレコードに存在しないIDをリクエストした際、例外をハンドリングする処理を実装。

--- src/main/java/com/example/catsdatabase/CatMapper.java
+++ src/main/java/com/example/catsdatabase/CatMapper.java
@@ -4,10 +4,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface CatMapper {
 
     @Select("SELECT * FROM cats WHERE coats LIKE CONCAT('%', #{coats}, '%')")
     List<Cat> findByCoats(String coats);
+
+    @Select("SELECT * FROM cats WHERE id = #{id}")
+    Optional<Cat> findById(Integer id);
 }
--- src/main/java/com/example/catsdatabase/CatService.java
+++ src/main/java/com/example/catsdatabase/CatService.java
@@ -3,6 +3,7 @@ package com.example.catsdatabase;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CatService {
@@ -17,4 +18,12 @@ public class CatService {
         return catMapper.findByCoats(coats);
     }
 
+    public Cat findCat(Integer id) {
+        Optional<Cat> cat = catMapper.findById(id);
+        if (cat.isPresent()) {
+            return cat.get();
+        } else {
+            throw new CatNotFoundException("そのIDは存在しません。");
+        }
+    }
 }
